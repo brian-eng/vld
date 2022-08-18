9dd89097b8a295666af5f70a39bf00a52251fda6
Bryan Moffit <moffit@jlab.org>
2022-08-17 23:23:48 -0400

    initial commit of skeleton

---
+++ vldLib.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+ * Copyright 2022, Jefferson Science Associates, LLC.
+ * Subject to the terms in the LICENSE file found in the top-level directory.
+ *
+ *     Authors: Bryan Moffit
+ *              moffit@jlab.org                   Jefferson Lab, MS-12B3
+ *              Phone: (757) 269-5660             12000 Jefferson Ave.
+ *              Fax:   (757) 269-5800             Newport News, VA 23606
+ *
+ * Description: Header for Driver Template
+ *
+ */
